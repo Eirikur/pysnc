33d9d4f61e0c42afb8a82485a4a50a9eaeaa8b5c
mozrat@gmail.com <mozrat@gmail.com@df7b51c3-683a-6720-a860-c100eae0140f>
2010-10-01 16:36:40 +0000

    First revision of PySNC.
    Functionality with this revision is the ability to retrieve Incidents from the Service Now Console by any attribute and to use that object (a PySNCIncident) to read it's details via the __dict__ object.
    
    sm@sm-laptop:~/Dropbox/code/pysnc$ python
    Python 2.6.5 (r265:79063, Apr 16 2010, 13:09:56) 
    [GCC 4.4.3] on linux2
    Type "help", "copyright", "credits" or "license" for more information.
    >>> import pysnc
    >>> p = pysnc.PySNC(username='simon', password='XXXXXX', instance='demo')
    >>> incident_list = p.filterIncidents(number='INC0013234')
    >>> incident_list
    [<pysnc.PySNCIncident instance at 0xb7630f2c>]
    
    >>> 
    >>> inc = incident_list[0]
    >>> 
    >>> inc.number
    'INC0013234'
    
    >>> inc.contact_type
    'email'
    
    >>> inc.sla_due
    '2010-09-15 00:00:00'

---
+++ trunk/pysnc.py
@@ -0,0 +1,85 @@
+""" A Python Library to query and update the Service Now console """
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+import copy
+import urllib
+import urllib2
+import simplejson as json
+
+# A base SNC Exception class
+class PySNCError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+class PySNCConfigError(PySNCError):
+    pass
+
+
+class PySNC:
+    """ The Service Now Console helper class """
+
+    def __init__(self, *args, **kwargs):
+        self.username = kwargs.get('username')
+        if not self.username: raise PySNCConfigError("Missing username parameter")
+
+        self.password = kwargs.get('password')
+        if not self.password: raise PySNCConfigError("Missing password parameter")
+
+        self.instance = kwargs.get('instance')
+        if not self.instance: raise PySNCConfigError("Missing instance parameter")
+
+        self.debug = kwargs.get('debug')
+        self.login()
+
+
+
+    def login(self):
+        """ Log into SNC and set the correct credentials"""
+
+        base_url = 'https://%s.service-now.com/' % self.instance
+        pass_manager = urllib2.HTTPPasswordMgrWithDefaultRealm()
+        pass_manager.add_password(None,base_url,self.username,self.password) 
+        authhandler = urllib2.HTTPBasicAuthHandler(pass_manager)
+
+        opener = urllib2.build_opener(authhandler)
+        urllib2.install_opener(opener)
+        if self.debug: '''Logged into Service Now and authenticated as %s''' % self.username
+
+
+    def get_incident_url(self, params):
+        """ Return the correct URL for querying the Service Now console Incident table """
+        str = ''
+        for k in params.keys():
+            str += '''%s=%s^''' % ( k, params[k])
+        str = str[:-1]
+        str = '''sysparm_action=getRecords&sysparm_query=%s''' % str
+        url = '''https://%s.service-now.com/incident.do?JSON&%s''' % ( self.instance, str )
+        if self.debug: logging.debug("Query URL %s" % url)
+        return url
+
+
+    def filterIncidents(self, *args, **kwargs):
+
+        if self.debug: logging.debug("Querying incident table with params %s" % kwargs)
+
+        url = self.get_incident_url(kwargs)
+        r = urllib2.urlopen(url)
+        j = json.loads(r.read())
+        if self.debug: logging.debug(j)
+        ret = []
+        for i in j['records']:
+            ret.append(PySNCIncident(snc_instance=self, data=i))
+        return ret
+                
+
+class PySNCIncident:
+    def __init__(self, snc_instance, data):
+        self.data = data
+        self.snc_instance = snc_instance
+        for k in self.data.keys():
+            self.__dict__[k] = data[k]
+            
+        
